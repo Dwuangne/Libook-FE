6ddaec830e3fb9c1ac97f7f61411463c5d70c955
Tuantrann24 <tranqtuan253264@gmail.com>
2024-09-22 13:05:39 +0700

    Footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import SignUp from "./components/general/SignUp";
 import SignIn from "./components/general/SignIn";
 import ProtectedRoute from "./gateway/RoleTransit";
-//import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import HomePage from "./components/general/homePage";
 import AdminLayout from "./components/admin/AdminLayout";
 import AdminHome from "./components/admin/AdminHome";
-//import HomePage from "./components/general/homePage";
-
 
 //import "./reset.css";
 import "./App.css";
@@ -17,24 +16,21 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
 
-
-        <Route element={<ProtectedRoute allowedRole="Customer" />}>
-
-        </Route>
+        <Route element={<ProtectedRoute allowedRole="Customer" />}></Route>
 
         <Route element={<ProtectedRoute allowedRole="Admin" />}>
           <Route path="/admin" element={<AdminLayout />}>
             <Route index element={<Navigate to={"/admin/dashboard"} />} />
             <Route path="dashboard" element={<AdminHome />} />
-          </Route> 
+          </Route>
         </Route>
       </Routes>
 
-     
+      <Footer />
       <ToastContainer />
     </div>
   );
